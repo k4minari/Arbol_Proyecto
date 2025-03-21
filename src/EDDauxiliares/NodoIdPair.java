c60dded62ce20f90b05aaccddc2fd165ba7d5378
Cesar Atencio <167229343+cesargatencio@users.noreply.github.com>
2025-03-21 08:47:25 -0400

    graphstream

---
+++ src/EDDauxiliares/NodoIdPair.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EDDauxiliares;
+
+import Logica.NodoArbol;
+
+/**
+ *
+ * @author cesar
+ */
+public class NodoIdPair {
+        public NodoArbol nodo;
+        public String id;
+
+        public NodoIdPair(NodoArbol nodo, String id) {
+            this.nodo = nodo;
+            this.id = id;
+        }
+    }

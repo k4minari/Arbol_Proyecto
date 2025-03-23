38dee5aba2cda6a59c3a3b6f99b6c6e1bd17acdd
Ildemar Pérez <168151340+k4minari@users.noreply.github.com>
2025-03-22 21:29:44 -0400

    locura

--- src/Interfaz/Directa.java
+++ src/Interfaz/Directa.java
@@ -3,7 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package Interfaz;
-
+import Logica.ArbolDicotomico;
+import Logica.TablaHash;
+import Logica.CalculoTiempos;
+import Logica.NodoArbol;
+import Logica.GraphStreamArbol; 
 /**
  *
  * @author ile1
@@ -11,9 +15,18 @@ package Interfaz;
 public class Directa extends javax.swing.JFrame {
 
     int mouseX, mouseY;
-    
+    TablaHash tabla = new TablaHash(50);  // PROBANDO (lo puedes borrar si lo necesitas)
     public Directa() {
         initComponents();
+        ArbolDicotomico arbol = new ArbolDicotomico(); // PROBANDO (lo puedes borrar si lo necesitas)
+
+      
+        arbol.seleccionarYcargarArchivo(); // PROBANDO (lo puedes borrar si lo necesitas)
+
+        
+        arbol.imprimirArbol(); // PROBANDO (lo puedes borrar si lo necesitas)
+ 
+        arbol.construirIndice(tabla); // PROBANDO (lo puedes borrar si lo necesitas)
     }
 
     /**
@@ -36,10 +49,11 @@ public class Directa extends javax.swing.JFrame {
         plantas1 = new javax.swing.JLabel();
         plantas2 = new javax.swing.JLabel();
         plantas3 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        selec = new javax.swing.JComboBox<>();
         plantas4 = new javax.swing.JLabel();
         tabla2 = new javax.swing.JLabel();
-        jPanel2 = new javax.swing.JPanel();
+        buscar = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
         tabla3 = new javax.swing.JLabel();
@@ -129,13 +143,12 @@ public class Directa extends javax.swing.JFrame {
         plantas3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/pngtree-branches-and-leaves-png-image_2423895-removebg-preview_2.png"))); // NOI18N
         jPanel1.add(plantas3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 320, -1, -1));
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+        selec.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox1ActionPerformed(evt);
+                selecActionPerformed(evt);
             }
         });
-        jPanel1.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 110, 110, 30));
+        jPanel1.add(selec, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 110, 110, 30));
 
         plantas4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/pngtree-branches-and-leaves-png-image_2423895-removebg-preview_3.png"))); // NOI18N
         jPanel1.add(plantas4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
@@ -143,20 +156,34 @@ public class Directa extends javax.swing.JFrame {
         tabla2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/fondo-madera-png-pisos-madera-dura_53876-627635-removebg-preview.png"))); // NOI18N
         jPanel1.add(tabla2, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 70, 300, 100));
 
-        jPanel2.setBackground(new java.awt.Color(0, 51, 0));
+        buscar.setBackground(new java.awt.Color(0, 51, 0));
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 230, Short.MAX_VALUE)
+        jButton1.setText("Buscar");
+        jButton1.setToolTipText("");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout buscarLayout = new javax.swing.GroupLayout(buscar);
+        buscar.setLayout(buscarLayout);
+        buscarLayout.setHorizontalGroup(
+            buscarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(buscarLayout.createSequentialGroup()
+                .addGap(69, 69, 69)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(66, Short.MAX_VALUE))
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 460, Short.MAX_VALUE)
+        buscarLayout.setVerticalGroup(
+            buscarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, buscarLayout.createSequentialGroup()
+                .addContainerGap(304, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(130, 130, 130))
         );
 
-        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 230, 460));
+        jPanel1.add(buscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 230, 460));
 
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
@@ -188,7 +215,12 @@ public class Directa extends javax.swing.JFrame {
         mouseX = evt.getX();
         mouseY = evt.getY();
     }//GEN-LAST:event_bgMousePressed
-
+    
+    public void guardarItem(){ // PROBANDO (lo puedes borrar si lo necesitas)
+        String[] especies = tabla.getAllKeys();
+        selec.addItem(especies);
+    
+    }
     private void bgMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bgMouseDragged
         int x = evt.getXOnScreen();
         int y = evt.getYOnScreen();
@@ -209,13 +241,22 @@ public class Directa extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_regresar2MouseClicked
 
-    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jComboBox1ActionPerformed
+    private void selecActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_selecActionPerformed
+        TablaHash tabla = new TablaHash(50);
+        tabla.getAllKeys();
+        
+    }//GEN-LAST:event_selecActionPerformed
 
     private void regresar3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_regresar3MouseClicked
         // TODO add your handling code here:
     }//GEN-LAST:event_regresar3MouseClicked
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        String especie = "";
+        
+ 
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -255,9 +296,9 @@ public class Directa extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel bg;
-    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JPanel buscar;
+    private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
     private javax.swing.JLabel plantas1;
@@ -268,6 +309,7 @@ public class Directa extends javax.swing.JFrame {
     private javax.swing.JLabel regresar1;
     private javax.swing.JLabel regresar2;
     private javax.swing.JLabel regresar3;
+    private javax.swing.JComboBox<String> selec;
     private javax.swing.JLabel tabla1;
     private javax.swing.JLabel tabla2;
     private javax.swing.JLabel tabla3;
